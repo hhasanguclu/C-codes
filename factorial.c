3b3f940110d5cfc46e6ff3fd98d980d0eb2ea255
hhasanguclu <54453870+hhasanguclu@users.noreply.github.com>
2020-01-11 00:57:39 +0300

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -4,20 +4,16 @@
 int factorial(int n)
 
 {
-
 	int result = 1;
 	for (int i = 1; i <= n;++i)
 	{
 		result *= i;
 	}
 	return result;
-
 }
 int main() {
-
 	for (int i = 0; i < 13; ++i)
 	{
 		printf("%2d! = %d\n", i, factorial(i));
 	}
-
 }
