32ed837cfdfbbbfd11c168357686ddb80c5e28f0
hhasanguclu <54453870+hhasanguclu@users.noreply.github.com>
2020-01-11 00:55:20 +0300

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int factorial(int n)
+
+{
+
+	int result = 1;
+	for (int i = 1; i <= n;++i)
+	{
+		result *= i;
+	}
+	return result;
+
+}
+int main() {
+
+	for (int i = 0; i < 13; ++i)
+	{
+		printf("%2d! = %d\n", factorial(i));
+	}
+
+}
